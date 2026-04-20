ac63efda7085e744368032e5b18829095e3f127a
Swarajya1998 <swarajyalaxmimeesala@gmail.com>
2026-04-20 11:13:05 +0530

    db changes

--- src/main/java/com/fuzentrix/backend/FuzentrixBackendApplication.java
+++ src/main/java/com/fuzentrix/backend/FuzentrixBackendApplication.java
@@ -2,11 +2,12 @@ package com.fuzentrix.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class })
 public class FuzentrixBackendApplication {
 
 	public static void main(String[] args) {
